e9f183739e9bfbdc46396453b4211798a6ed2876
manotendulkar <manotendulkar10@gmail.com>
2024-03-07 15:10:04 +0530

    create redux store

---
+++ src/utils/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+
+const headers = {
+  Authorization: "bearer " + TMDB_TOKEN,
+};
+
+export const fetchDataFromApi = async (url, params) => {
+  try {
+    const { data } = await axios.get(BASE_URL + url, {
+      headers: headers, //api.js headers
+      params: params, //params getting from other
+    });
+    return data;
+  } catch (error) {
+    console.log("API Error: ", error);
+    return error;
+  }
+};
