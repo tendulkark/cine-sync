a5f7a5b2d2eb30edce0def02c36e374215af695f
manotendulkar <manotendulkar10@gmail.com>
2024-03-08 21:08:08 +0530

    modify

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>
 );
