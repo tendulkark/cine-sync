bddf63d6b3f3af0330b53fd2cf4d15f6037f612a
manotendulkar <manotendulkar10@gmail.com>
2024-03-11 21:56:58 +0530

    new folders add

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import { fetchDataFromApi } from "./utils/api";
 import { useDispatch, useSelector } from "react-redux";
 import { getApiConfiguration, selectAllHome } from "./store/homeSlice";
 
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Details from "./pages/details/Details";
+import Explore from "./pages/explore/Explore";
+import Home from "./pages/home/Home";
+import SearchResults from "./pages/searchResults/SearchResults";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PageNotFound from "./pages/404/PageNotFound";
+
 function App() {
   const dispatch = useDispatch();
   const { url } = useSelector(selectAllHome);
@@ -19,10 +28,17 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div className="App">App {url?.total_pages}</div>
-      <div>Hello</div>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResults />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
