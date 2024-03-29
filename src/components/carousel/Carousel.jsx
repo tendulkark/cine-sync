ee4ad5ae15fa1d86a25061a6904fb3afb11d7ef6
manotendulkar <manotendulkar10@gmail.com>
2024-03-29 15:45:13 +0530

    add carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -14,7 +14,68 @@ import PosterFallback from "../../assets/no-poster.png";
 import "./style.scss";
 
 const Carousel = ({ data, loading }) => {
-  return <div>Carousel</div>;
+  const carouselContainer = useRef();
+  const { url } = useSelector((state) => state.home);
+  const navigate = useNavigate();
+
+  const navigation = (direction) => {};
+
+  const skeletonItem = () => {
+    return (
+      <div className="skeletonItem">
+        <div className="posterBlock skeleton"></div>
+        <div className="textBlock">
+          <div className="title skeleton"></div>
+          <div className="date skeleton"></div>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div className="carousel">
+      <ContentWrapper>
+        <BsFillArrowLeftCircleFill
+          className="carouselLeftNav arrow"
+          onClick={() => navigation("left")}
+        />
+        <BsFillArrowRightCircleFill
+          className="carouselRightNav arrow"
+          onClick={() => navigation("right")}
+        />
+
+        {!loading ? (
+          <div className="carouselItems">
+            {data?.map((item) => {
+              const posterUrl = item.poster_path
+                ? url.poster + item.poster_path
+                : PosterFallback;
+              return (
+                <div className="carouselItem" key={item.id}>
+                  <div className="posterBlock">
+                    <Img src={posterUrl} />
+                  </div>
+                  <div className="textBlock">
+                    <span className="title">{item.title || item.name}</span>
+                    <span className="date">
+                      {dayjs(item.release_date).format("MMM D, YYYY")}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="loadingSkeleton">
+            {skeletonItem()}
+            {skeletonItem()}
+            {skeletonItem()}
+            {skeletonItem()}
+            {skeletonItem()}
+          </div>
+        )}
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default Carousel;
