77b0a19b4ad98cef230ac6e0ee1eee43b66f35c5
manotendulkar <manotendulkar10@gmail.com>
2024-04-03 21:39:14 +0530

    add detailsBanner

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./style.scss";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 
 const Details = () => {
-  return <div>Details</div>;
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}`);
+  // console.log("Details", data?.results);
+  return (
+    <div>
+      Details
+      <DetailsBanner />
+    </div>
+  );
 };
 
 export default Details;
