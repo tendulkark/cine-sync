5b99dc3fcd6e24d3242f93fd000f6e6af8a3f847
manotendulkar <manotendulkar10@gmail.com>
2024-04-04 12:00:57 +0530

    update detailsbanner

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -6,12 +6,14 @@ import DetailsBanner from "./detailsBanner/DetailsBanner";
 
 const Details = () => {
   const { mediaType, id } = useParams();
-  const { data, loading } = useFetch(`/${mediaType}/${id}`);
-  // console.log("Details", data?.results);
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
+
   return (
     <div>
-      Details
-      <DetailsBanner />
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
     </div>
   );
 };
--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -11,12 +11,20 @@ import Genres from "../../../components/genres/Genres";
 import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/lazyLoadImg/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
+import { PlayIcon } from "./PlayBtn.jsx";
 
 const DetailsBanner = ({ video, crew }) => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
 
   const { url } = useSelector((state) => state.home);
+
+  const _genres = data?.genres?.map((g) => g.id);
+
+  const director = crew?.filter((m) => m.job === "Director");
+  const writer = crew?.filter(
+    (m) => m.job === "Writer" || m.job === "Screenplay" || m.job === "Story"
+  );
 
   const toHoursAndMinutes = (totalMinutes) => {
     const hours = Math.floor(totalMinutes / 60);
@@ -46,7 +54,93 @@ const DetailsBanner = ({ video, crew }) => {
                         <Img className="posterImg" src={PosterFallback} />
                       )}
                     </div>
-                    <div className="right"></div>
+                    <div className="right">
+                      <div className="title">
+                        {data?.name || data?.title}{" "}
+                        {dayjs(data?.release_date).format("(YYYY)")}
+                      </div>
+                      <div className="subtitle">{data?.tagline}</div>
+                      <div className="genres">
+                        <Genres data={_genres} />
+                      </div>
+                      <div className="row">
+                        <CircleRating rating={data?.vote_average.toFixed(1)} />
+                        <div className="playbtn" onClick={() => {}}>
+                          <PlayIcon />
+                          <span className="text">Watch Trailer</span>
+                        </div>
+                      </div>
+                      <div className="overview">
+                        <div className="heading">Overview</div>
+                        <div className="description">{data?.overview}</div>
+                      </div>
+                      <div className="info">
+                        {data.status && (
+                          <div className="infoItem">
+                            <span className="text bold">Status: </span>
+                            <span className="text">{data.status}</span>
+                          </div>
+                        )}
+                        {data.release_date && (
+                          <div className="infoItem">
+                            <span className="text bold">Release Date: </span>
+                            <span className="text">
+                              {dayjs(data.release_date).format("MMM D, YYYY")}
+                            </span>
+                          </div>
+                        )}
+                        {data.runtime && (
+                          <div className="infoItem">
+                            <span className="text bold">Runtime: </span>
+                            <span className="text">
+                              {toHoursAndMinutes(data.runtime)}
+                            </span>
+                          </div>
+                        )}
+                      </div>
+
+                      {director?.length > 0 && (
+                        <div className="info">
+                          <span className="text bold">Director:</span>
+                          <span className="text">
+                            {director?.map((person, index) => (
+                              <span key={index}>
+                                {person.name}
+                                {director.length - 1 !== index && ", "}
+                              </span>
+                            ))}
+                          </span>
+                        </div>
+                      )}
+
+                      {writer?.length > 0 && (
+                        <div className="info">
+                          <span className="text bold">Writer:</span>
+                          <span className="text">
+                            {writer?.map((person, index) => (
+                              <span key={index}>
+                                {person.name}
+                                {writer.length - 1 !== index && ", "}
+                              </span>
+                            ))}
+                          </span>
+                        </div>
+                      )}
+
+                      {data?.created_by?.length > 0 && (
+                        <div className="info">
+                          <span className="text bold">Creator:</span>
+                          <span className="text">
+                            {data?.created_by?.map((person, index) => (
+                              <span key={index}>
+                                {person.name}
+                                {data?.created_by?.length - 1 !== index && ", "}
+                              </span>
+                            ))}
+                          </span>
+                        </div>
+                      )}
+                    </div>
                   </div>
                 </ContentWrapper>
               </div>
