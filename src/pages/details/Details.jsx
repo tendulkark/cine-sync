bf03711ffa710a5b1ada674be2097e9a5bce396e
manotendulkar <manotendulkar10@gmail.com>
2024-04-04 23:12:12 +0530

    add videosSection, cast, videoPopoup

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,8 @@ import "./style.scss";
 import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/videosSection";
 
 const Details = () => {
   const { mediaType, id } = useParams();
@@ -14,6 +16,8 @@ const Details = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={loading} />
+      <VideosSection data={data?.results} loading={loading} />
     </div>
   );
 };
